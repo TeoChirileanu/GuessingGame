26613309ac89961ee6dc141de38dd5ca9b1998a9
Teodor Chirileanu <teodor@mobibam.com>
2018-12-02 15:11:37 +0100

    added business rules

---
+++ BusinessRules/NumberChecker.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Common;
+
+namespace BusinessRules {
+    public class NumberChecker : INumberChecker {
+        private readonly int CorrectNumber;
+
+        public NumberChecker(int correctNumber) => CorrectNumber = correctNumber;
+
+        public NumberChecker() => CorrectNumber = Resources.CorrectNumber;
+
+        public string CheckNumber(int number) {
+            NumberValidator.ValidateNumber(number);
+            var comparisonResult = number.CompareTo(CorrectNumber);
+            switch (comparisonResult) {
+                case -1:
+                    return Resources.TooLowMessage;
+                case 1:
+                    return Resources.TooHighMessage;
+                case 0:
+                    return Resources.CorrectMessage;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+    }
+}
---
+++ BusinessRules/NumberValidator.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Common;
+
+namespace BusinessRules {
+    public static class NumberValidator {
+        public static void ValidateNumber(int number) {
+            if (number < Resources.LowerBound || number > Resources.UpperBound)
+                throw new ArgumentOutOfRangeException(Resources.NumberValidatorExceptionMessage);
+        }
+    }
+}
