398d0e954158020b5a6f25abecea5432ea46bcb9
Teodor Chirileanu <teodorchirileanu@gmail.com>
2018-12-16 17:46:31 +0000

    Merged PR 9: Removed resharper
    Related work items: #3

--- Tools/Resharper/JavaScript/Linters/Main.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Logging_1 = require("./Logging");
-var loader = require("./LintersLoader");
-var LintSession_1 = require("./LintSession");
-var logger = new Logging_1.ErrorLogger();
-var linterFactories = loader.getLinterFactories(logger);
-var session = new LintSession_1.default(logger, linterFactories);
-session.start();
