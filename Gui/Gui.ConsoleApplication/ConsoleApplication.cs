97542a323e2d6471b5eefc6b03075c3ed672e53e
Teodor Chirileanu <teodorchirileanu@gmail.com>
2018-12-19 20:21:00 +0000

    Merged PR 12: added console gui
    added console gui
    
    Related work items: #7

---
+++ Gui/Gui.ConsoleApplication/ConsoleApplication.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using BusinessRules;
+using Common;
+using Infrastructure;
+using UseCases;
+
+namespace Gui.ConsoleApplication
+{
+    public static class ConsoleApplication
+    {
+        public static void Main() {
+            IGuessedNumberGetter stdinGuessedNumberGetter = new StdinGuessedNumberGetter();
+            INumberChecker numberChecker = new NumberChecker();
+            IGuessResultDeliverer guessResultDeliverer = new StdoutGuessResultDeliverer();
+            
+            var guessFacade = new GuessFacade(stdinGuessedNumberGetter, numberChecker, guessResultDeliverer);
+            Run(guessFacade);
+        }
+
+        private static void Run(IGuessFacade guessFacade)
+        {
+            var gameOver = false;
+            do
+            {
+                int? guessedNumber = guessFacade.GetGuessedNumber();
+                if (guessedNumber == null) continue;
+                var guessResult = guessFacade.CheckGuessedNumber(guessedNumber.Value);
+                if (guessResult.Equals(Resources.CorrectMessage)) gameOver = true;
+                guessFacade.DeliverGuessResult(guessResult);
+            } while (!gameOver);
+        }
+    }
+}
