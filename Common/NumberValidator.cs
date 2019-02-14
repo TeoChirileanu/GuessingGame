5fa5dd08c428827a8411887d98fddfb78003a132
Teodor Chirileanu <teodorchirileanu@gmail.com>
2019-02-14 20:46:35 +0000

    Merged PR 32: Added new validator
    Related work items: #47

--- Common/NumberValidator.cs
+++ Common/NumberValidator.cs
@@ -6,5 +6,12 @@ namespace Common {
             if (number < Resources.LowerBound || number > Resources.UpperBound)
                 throw new ArgumentOutOfRangeException(Resources.NumberOutOfBoundsMessage);
         }
+
+        public static int? GetIntValue(this string numberAsString) {
+            var parsedSuccessfully = int.TryParse(numberAsString, out var parsedNumber);
+            if (!parsedSuccessfully) return null;
+
+            return parsedNumber;
+        }
     }
 }
---
+++ Tests/Common.UnitTests/NumberValidatorTests.cs
@@ -0,0 +1,53 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NFluent;
+
+namespace Common.UnitTests
+{
+    [TestClass]
+    public class NumberValidatorTests
+    {
+        private static string NumberAsString { get; set; }
+
+        [DataRow(null)]
+        [DataRow("")]
+        [DataRow("\r\n\t")]
+        [TestMethod]
+        public void GetIntValue_NullOrWhitespace_null(string value)
+        {
+            // Arrange
+            NumberAsString = value;
+
+            // Act
+            int? actualResult = NumberAsString.GetIntValue();
+
+            // Assert
+            Check.That(actualResult).Equals(null);
+        }
+
+        [TestMethod]
+        public void GetIntValue_ValidNumber_Number()
+        {
+            // Arrange
+            NumberAsString = "123";
+
+            // Act
+            int? actualResult = NumberAsString.GetIntValue();
+
+            // Assert
+            Check.That(actualResult).Equals(123);
+        }
+
+        [TestMethod]
+        public void GetIntValue_InvalidNumber_Null()
+        {
+            // Arrange
+            NumberAsString = "asd";
+
+            // Act
+            int? actualResult = NumberAsString.GetIntValue();
+
+            // Assert
+            Check.That(actualResult).Equals(null);
+        }
+    }
+}
